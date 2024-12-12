23a569b2b8530f8c3b171d228d67199d786bc25f
mayrars <mayra203@gmail.com>
2024-12-12 07:12:06 -0600

    Import mogoose and created getall entries

--- controllers/entryController.js
+++ controllers/entryController.js
@@ -1,6 +1,19 @@
-const getAllEntries = async((req, res)=>{
-    
-})
+const Entry = require('../models/entryModel')
+const getAllEntries = async(req, res) => {
+    const {page} = req.query
+    const entryPerPage = 10
+    try{
+        if(page<=1){
+            pageNum = 0
+        }else{
+            pageNum = page-1
+        }
+        const result = await Entry.find().sort({createdAt: -1}).skip(pageNum*entryPerPage).limit(entryPerPage)
+        res.status(200).json({success: true, message: 'Entries', data: result})
+    }catch(error){
+        console.log(error)
+    }
+}
 
 const getSingleEntry = async((req, res)=>{
     
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require('cors');
+const mongose = require('mongoose');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,6 +9,11 @@ const PORT = process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+mongose.connect(process.env.MONGO_URI).then(() => {
+    console.log('connected to db')
+}).catch((err) => {
+    console.log(err)
+})
 
 app.listen(PORT), () => {
     console.log('listen...')
