ba60506a65a53160e5dad3498a231728c55abd7c
mayrars <mayra203@gmail.com>
2024-12-16 05:52:44 -0600

    Changed model schema

--- models/entryModel.js
+++ models/entryModel.js
@@ -1,29 +1,38 @@
-const mongoose = require("mongoose")
-const entrySchema = mongoose.Schema({
+const mongoose = require("mongoose");
+
+// Pronunciation schema
+const pronunciationSchema = new mongoose.Schema({
+    audio: {
+        type: String,
+        trim: true,
+    },
+    sourceUrl: {
+        type: String,
+        trim: true,
+    },
+});
+//Meanings schema
+const meaningSchema = new mongoose.Schema({
+    definitions: {
+        type: [String], 
+        required: [true, 'At least one definition is required'],
+        trim: true,
+    },
+    examples: {
+        type: String,
+        trim: true,
+    },
+});
+
+// principal schema
+const entrySchema = new mongoose.Schema({
     word: {
         type: String,
         required: [true, 'Word is required'],
         trim: true,
     },
-    pronunciation:[{
-      audio: {
-        type:String,
-        trim: true,
-      },
-      sourceUrl:{
-        type:String,
-        trim: true,
-      }  
-    }], 
-    meanings: [{
-        definitions:{
-            type: String,
-            required: [true, 'Meaning is required'],
-            trim: true,
-        },
-        example:{
-            type: String,
-            trim: true,
-        }
-    }]
-})
+    pronunciation: [pronunciationSchema], 
+    meanings: [meaningSchema], 
+});
+
+module.exports = mongoose.model("Entry", entrySchema);
